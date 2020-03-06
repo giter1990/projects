b2dad78de3df9bb85a04556b82f77613a02f7b78
Jenia Ivanenko <vodoleichik11.02.1990@gmail.com>
2020-03-06 23:06:50 +0200

    changed 6.03.2020

--- js/script.js
+++ js/script.js
@@ -87,6 +87,65 @@ window.addEventListener("DOMContentLoaded", function() {
 	}
 	
 	setClock("timer", deadline);
+	
+	// Модальное окно
+	let more = document.querySelector(".more"),
+		overlay = document.querySelector(".overlay"),
+		close = document.querySelector(".popup-close"),
+		descriptionBtn = document.querySelector(".description-btn"),
+		health = document.querySelector(".health"),
+		blackout = document.querySelector(".blackout"),
+		infoBlock = document.querySelector(".info");
+	
+	more.addEventListener("click", function() {
+		overlay.style.display = "block";
+		this.classList.add("more-splash");
+		document.body.style.overflow = "hidden";
+	});
+	
+	close.addEventListener("mouseover", function() {
+		this.style.textShadow = "0 0 5px #fff";
+	});
+	
+	close.addEventListener("mouseout", function() {
+		this.style.textShadow = "none";
+	});
+	
+	overlay.addEventListener("click", function(e) {
+		if (e.target && e.target.classList.contains("popup-close")) {
+			this.style.display = "none";
+			more.classList.remove("more-splash");
+			document.body.style.overflow = "";
+		}
+	});
+	
+	/* close.addEventListener("click", function() { // не работает с двумя крестиками (окнами) (см. коммент к стр. 134)
+		overlay.style.display = "none";
+		more.classList.remove("more-splash");
+		document.body.style.overflow = "";
+	}); */
+	
+	descriptionBtn.addEventListener("click", function() {
+		this.style.transform = "scale(1.1)";
+		blackout.style.display = "block";
+		health.style.display = "block";
+	});
+	
+	infoBlock.addEventListener("click", function(e) { // одинаковое окно для всех табов
+		if (e.target && e.target.classList.contains("description-btn")) {
+			this.style.transform = "scale(1.1)";
+			blackout.style.display = "block";
+			health.style.display = "block";
+		}
+	});
+	
+	health.addEventListener("click", function(e) {
+		if (e.target && e.target.classList.contains("popup-close")) { // прописываем родителя и ребёнка, содержащего класс крестика (то же делаем и для overlay, стр. 114)
+			this.style.display = "none";
+			more.classList.remove("more-splash");
+			document.body.style.overflow = "";
+		}
+	});
 });
 
 
