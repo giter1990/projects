39412acee0a081b033a2bcb918498b575c265b9d
Jenia Ivanenko <vodoleichik11.02.1990@gmail.com>
2020-03-11 20:31:15 +0200

    changed 11.03.2020

--- js/script.js
+++ js/script.js
@@ -3,24 +3,23 @@ window.addEventListener("DOMContentLoaded", function() {
 	
 	let tab = document.querySelectorAll(".info-header-tab"),
 		info = document.querySelector(".info-header"),
-		tabContent = document.querySelectorAll(".info-tabcontent");
-		
-	function hideTabContent(a) {
-		for (let i = a; i < tabContent.length; i++) {
-			tabContent[i].classList.remove("show");
-			tabContent[i].classList.add("hide");
-		}
-	};
+		tabContent = document.querySelectorAll(".info-tabcontent"),
+		hideTabContent = (a) => { // ES6
+			for (let i = a; i < tabContent.length; i++) {
+				tabContent[i].classList.remove("show");
+				tabContent[i].classList.add("hide");
+			}
+		},
+		showTabContent = (b) => { // ES6
+			if (tabContent[b].classList.contains("hide")) {
+				tabContent[b].classList.remove("hide");
+				tabContent[b].classList.add("show");
+			}
+		};
 	
 	hideTabContent(1); // скрытие всех tabContent, кроме первого (с индексом 0)
 	
-	function showTabContent(b) {
-		if (tabContent[b].classList.contains("hide")) {
-			tabContent[b].classList.remove("hide");
-			tabContent[b].classList.add("show");
-		}
-	};
-	
+	// Перебирание табов
 	info.addEventListener("click", function(e) { 
 		let target = event.target;
 		
@@ -33,71 +32,49 @@ window.addEventListener("DOMContentLoaded", function() {
 				}
 			}
 		}
-	}); // e надо для сравнения с тем, куда кликаем (стр. 24)
+	}); // e надо для сравнения с тем, куда кликаем (стр. 37)
 	
 	// Таймер обратного отсчёта
-	let deadline = "2020-03-14";
-	
-	function getTimeRemaining(endtime) {
-		let t = Date.parse(endtime) - Date.parse(new Date()),
+	let deadline = "2020-03-14",
+		getTimeRemaining = (endtime) => { // ES6
+			let t = Date.parse(endtime) - Date.parse(new Date()),
 			seconds = Math.floor((t / 1000) % 60), // получаем кол-во секунд (работает и без Math.floor)
 			minutes = Math.floor((t / 1000 / 60) % 60),
 			hours = Math.floor(t / (1000 * 60 * 60));
-			// hours = Math.floor((t / 1000 / 60 / 60) % 24), другой формат получения кол-ва часов
-			// days = Math.floor(t / (1000 * 60 * 60 * 24));
 		
-		return { // извлечение общего времени и ч, м, с
-			"total": t,
-			"hours": hours,
-			"minutes": minutes,
-			"seconds": seconds
-		}
-	}
-	
-	function setClock(id, endtime) { // изменение файла HTML
-		let timer = document.getElementById(id), // выставление и запуск часов
+			return { // извлечение общего времени и ч, м, с
+				"total": t,
+				"hours": hours,
+				"minutes": minutes,
+				"seconds": seconds
+			}
+		},
+		setClock = (id, endtime) => { // изменение файла HTML (ES6)
+			let timer = document.getElementById(id), // выставление и запуск часов
 			hours = timer.querySelector(".hours"),
 			minutes = timer.querySelector(".minutes"),
 			seconds = timer.querySelector(".seconds"),
+			updateClock = () => {
+				let t = getTimeRemaining(endtime),
+					addZero = (num) => { 
+						if (num <= 9) {
+							return '0' + num;
+						} else return num;
+					};
+				
+				hours.textContent = addZero(t.hours);
+				minutes.textContent = addZero(t.minutes);
+				seconds.textContent = addZero(t.seconds);
+				
+				if (t.total <= 0) {
+					clearInterval(timeInterval);
+					hours.textContent = '00';
+					minutes.textContent = '00';
+					seconds.textContent = '00';
+				}
+			},
 			timeInterval = setInterval(updateClock, 1000);
-			
-		function updateClock() { // вызов функции каждую секунду
-			let t = getTimeRemaining(endtime);
-			
-			/* if (t.hours < 10) { // 1-й способ учёта нуля (стр. 67-81)
-				t.hours = "0" + t.hours;
-			}
-			
-			if (t.minutes < 10) {
-				t.minutes = "0" + t.minutes;
-			}
-			
-			if (t.seconds < 10) {
-				t.seconds = "0" + t.seconds;
-			} */
-			
-			/* hours.textContent = t.hours; // запись ч, м, с, полученная из объекта (стр. 50 - 53)
-			minutes.textContent = t.minutes;
-			seconds.textContent = t.seconds; */
-			
-			function addZero(num){ // 2-й способ учёта нуля (стр. 83-94)
-				if (num <= 9) {
-					return '0' + num;
-				} else return num;
-			};
-			
-			hours.textContent = addZero(t.hours);
-            minutes.textContent = addZero(t.minutes);
-            seconds.textContent = addZero(t.seconds);
-			
-			if (t.total <= 0) {
-				clearInterval(timeInterval);
-				hours.textContent = '00';
-                minutes.textContent = '00';
-                seconds.textContent = '00';
-			}
-		}
-	}
+		};
 	
 	setClock("timer", deadline);
 	
@@ -132,12 +109,6 @@ window.addEventListener("DOMContentLoaded", function() {
 		}
 	});
 	
-	/* close.addEventListener("click", function() { // не работает с двумя крестиками (окнами) (см. коммент к стр. 134)
-		overlay.style.display = "none";
-		more.classList.remove("more-splash");
-		document.body.style.overflow = "";
-	}); */
-	
 	descriptionBtn.addEventListener("click", function() {
 		this.style.transform = "scale(1.1)";
 		blackout.style.display = "block";
@@ -153,7 +124,7 @@ window.addEventListener("DOMContentLoaded", function() {
 	});
 	
 	health.addEventListener("click", function(e) {
-		if (e.target && e.target.classList.contains("popup-close")) { // прописываем родителя и ребёнка, содержащего класс крестика (то же делаем и для overlay, стр. 114)
+		if (e.target && e.target.classList.contains("popup-close")) {
 			this.style.display = "none";
 			more.classList.remove("more-splash");
 			document.body.style.overflow = "";
